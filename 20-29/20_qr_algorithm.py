659cd7e250373000787cbadb99be35668bb9a05c
Michal Burgunder <michal_burgunder@yahoo.com>
2022-12-14 15:21:19 +0100

    first complete version QR algorithm

--- 20-29/20_qr_algorithm.py
+++ 20-29/20_qr_algorithm.py
@@ -1,4 +1,5 @@
 import numpy as np
+import copy
 
 def proj(un, an):
     return ((un.T@an)/(un.T@un))*un
@@ -6,43 +7,31 @@ def proj(un, an):
 def qr_decomposition(matrix):
     U = [matrix[:,0]]
     for i in range(1, len(matrix)):
-        un = matrix[:,i]
+        un = copy.deepcopy(matrix[:,i])
         for j in range(i, 0, -1):
-            un -= proj(np.array(U[j-1], dtype=float), np.array(matrix[:,i],dtype=float))
+            un -= proj(np.array(U[j-1]), np.array(matrix[:,i]))
         U.append(un)
-    U = np.array(U, dtype=float).T
+    U = np.array(U).T
 
     Q = []
     for i in range(0,len(U)):
-        Q.append(U[:,i]/np.linalg.norm(U[:,i]))
+        Q.append(U[:,i]/np.linalg.norm(U[:,i])*(-1))
         
     Q = np.array(Q, dtype=float).T
     R = Q.T@matrix
-    # print(matrix)
-    # print(Q)
-    # print(R)
-    # exit()
+
     return Q, R
 
-def qr_algorithm(A, max_iter=10):
-    
-    # Initialize values
-    q = np.eye(A.shape[0]) # this is just the identity matrix
-    r = q.copy() # ..
-    
-    a = A
-    for i in range(0, max_iter):
-        print(a)
-        q, r = qr_decomposition(a)
-        # print(q)
-        # print(r)
-        # print(q@r)
-        # exit()
-        # q, r = np.linalg.qr(a)
-        a = np.dot(r, q)
-        # a = q.T@a@q
 
-    return np.diagonal(a)
+def qr_algorithm(A, max_iter=100):
+    q = A.copy()# this is just the identity matrix
+    r = A.copy() # ..
+    
+    for i in range(0, max_iter):
+        q, r = qr_decomposition(r)
+        r = np.dot(r, q)
+
+    return np.diagonal(r)
 
 matrix = np.array([[12,-51,4], [6,167,-68], [-4,24,-41]], dtype=float)
 
@@ -50,11 +39,3 @@ res = qr_algorithm(matrix)
 
 print(res)
 print(np.linalg.eigvals(matrix))
-
-            
-        
-        
-        
-        
-# print(qr_algorithm(matrix))
-# print(qr_decomposition(matrix))

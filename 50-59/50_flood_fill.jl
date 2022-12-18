45e049509088055838cc88a0bf0be2d29d5ce362
Michal Burgunder <michal_burgunder@yahoo.com>
2022-12-18 20:47:56 +0100

    flood fill clean up

--- 50-59/50_flood_fill.jl
+++ 50-59/50_flood_fill.jl
@@ -5,29 +5,24 @@ function get_neighboring_points(matrix, p, c)
     if p[1] != 1 && getindex(matrix, p[1]-1, p[2]) == c
         push!(points_to_process, (p[1]-1, p[2]))
     end
-    # print("here")
+
     if p[2] != 1 && getindex(matrix, p[1], p[2]-1) == c
         push!(points_to_process, (p[1], p[2]-1))
     end
 
-    # print()
-    # size(matrix)[0]
     if p[1] != size(matrix)[1] && getindex(matrix, p[1]+1, p[2]) == c
         push!(points_to_process, (p[1]+1, p[2]))
     end
-    # print("3", "\n")
+
     if p[2] != size(matrix)[2] && getindex(matrix, p[1], p[2]+1) == c
         push!(points_to_process, (p[1], p[2]+1))
     end
-    # print(points_to_process)
-    # exit()
+
     return points_to_process
 end
 
 function inner_fill(dones, p, matrix, new_color, old_color)
-    # println("in inner fill")
     if getindex(dones, p[1], p[2]) == 1
-        # println("returning")
         return
     end
 
@@ -36,13 +31,9 @@ function inner_fill(dones, p, matrix, new_color, old_color)
     matrix[p[1], p[2]] = new_color
     
     new_points = get_neighboring_points(matrix, p, old_color)
-    # print(new_points)
-    # exit()
+
     for point in new_points
-        println("going in")
-        println(point)
         inner_fill(dones, point, matrix, new_color, old_color)
-        println("coming out in")
     end
 end
 
@@ -63,12 +54,17 @@ function flood_fill(matrix, point, new_color)
     return an_H
 end
 
+function print_matrix(matrix)
+    for row in eachrow(an_H)
+        println(row)
+    end
+end
+
 an_H = [
     0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0;0 0 1 0 0 1 0 0;0 0 1 0 0 1 0 0;0 0 1 1 1 1 0 0;0 0 1 0 0 1 0 0;0 0 1 0 0 1 0 0;0 0 0 0 0 0 0 0;0 0 0 0 0 0 0 0
 ]
-# print(an_H)
+print_matrix(an_H)
+println("------------------------")
 flood_fill(an_H, (1,1), 6)
+print_matrix(an_H)
 
-for row in eachrow(an_H)
-    println(row)
-end

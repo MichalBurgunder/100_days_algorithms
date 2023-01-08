aac2aa423ff658d04f00ad96d5247b92b5fcb4bb
Michal Burgunder <michal_burgunder@yahoo.com>
2023-01-08 12:55:11 +0100

    first final version of CORDIC

---
+++ 20-29/28_CORDIC.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+// custom function to multiply a matrix by a vector
+vector<float> mat_mult(vector<vector<float>> A, vector<float> B)
+{
+    // Initialize the result matrix with zeros
+    vector<float> C = {0, 0};
+
+
+    C[0] += A[0][0] * B[0];
+    C[0] += A[0][1] * B[1];
+
+    C[1] += A[1][0] * B[0];
+    C[1] += A[1][1] * B[1];
+
+    return C;
+}
+
+// returns our appropriate sigma value, based on the angle
+int sigma_i(float angle) {
+    if(angle > 0) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+// we return our K value based on i
+float K_i(int i) {
+    return 1/(sqrt(1+pow(2, -2*i)));
+}
+
+vector<vector<float> > R_i(int i, int s) {
+    vector<vector<float> > A(2, vector<float>(2));
+    A[0][0] = 1;
+    A[0][1] = -s*pow(2, -i);
+    A[1][0] = s*pow(2, -i);
+    A[1][1] = 1;
+    return move(A);
+}
+
+float gamma_i(int i) {
+     return atan(pow(2, -i));
+}
+
+int main()
+{
+    vector<float> v = {1, 0};
+
+    int n = 20;
+    float angle = 5*M_PI/17;
+    cout << "\ncmath computed values for cosine and sine: " << endl;
+    cout << "cosine: " << cos(angle) << endl;
+    cout << "sine: " << sin(angle) << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        int s = sigma_i(angle);
+        float k = K_i(i);
+        vector<vector<float> > r = R_i(i, s);
+        r[0][0] *= k;
+        r[0][1] *= k;
+        r[1][0] *= k;
+        r[1][1] *= k;
+        v = mat_mult(r, v);
+        angle = angle - (s * gamma_i(i));
+    }
+    cout << "\nCORDIC computed values for cosine and sine:" << endl;
+    cout << "cosine: " << v[0] << endl;
+    cout << "sine: " << v[1] << endl;
+
+
+
+    return 0;
+}
--- _archive/28_CORDIC.py
+++ _archive/28_CORDIC.py
@@ -19,7 +19,6 @@ def R(i, s):
         [s*(2**(-i)), 1]
         ])
 
-
 original_angle = 5*math.pi/17 # set
 angle = copy.copy(original_angle)
 
@@ -31,7 +30,7 @@ for i in range(0,n):
     s = sigma_i(angle)
     v = K_i(i)*R(i,s)@v
     angle = angle - (s * gamma_i(i))
-#     print(angle)
+
 print("v")
 print(v)
 print("via math")

084810d186d90620a247d4e5f81fbad39b40b757
Michal Burgunder <michal_burgunder@yahoo.com>
2023-01-14 09:44:30 +0100

    computed forces barnes hut

--- 10-19/12_barnes_hut.jl
+++ 10-19/12_barnes_hut.jl
@@ -4,7 +4,11 @@ struct Point
     mass::Float64
 end
 
+# will denote those trees that have not been initialized yet
 block_point = Point(-1,-1,-1)
+G = 6.674*10^(-11) # gravitational constant
+
+
 mutable struct QuadTree
     squares::Dict{String, Union{QuadTree, Nothing}}
     point::Union{Point, Nothing}
@@ -14,8 +18,6 @@ mutable struct QuadTree
 
     v_start::Float64
     h_start::Float64
-
-
 end
 
 squares = ["ul", "ur", "dl", "dr"]
@@ -27,7 +29,6 @@ function barycenter(quad::QuadTree)
     top = [0, 0]
     bot = 0
     for i in 1:length(squares)
-        # println(quad.squares[squares[i]])
         if quad.squares[squares[i]] != nothing
             barycenter(quad.squares[squares[i]])
 
@@ -84,17 +85,11 @@ function set_new_quad_tree(quad::QuadTree, point::Point, ud::String,lr::String)
         "dr" => nothing
         )
     quad.squares[ud*lr] = QuadTree(empty_dict, point, box[1], box[2], box[3], box[4])
-    quad.point = block_point
+    quad.point = block_point # an impossible point, so we block any processing here
 end
 
 l = 0
 function insert(quad::QuadTree, point::Point)
-    # if l == 3
-    #     println("exiting...")
-    #     exit()
-    # end
-    # global l += 1
-
     # very first point
     if quad.point == nothing
         quad.point = point
@@ -129,13 +124,63 @@ function insert(quad::QuadTree, point::Point)
     global l -= 1
 end
 
-function compute_forces(points::Array{Point}, quadTree::QuadTree)
+function euclidean_dis(p1, p2)
+    return sqrt((p1.x - p2.x)^2 + (p1.y - p2.y)^2)
+end
+
+function attractive_force(p1::Point, p2::Point, dis::Float64)
+    # normalize distances for x and y for unit vector
+    x = (p1.x + p2.x)/dis
+    y = (p1.y + p2.y)/dis
+
+    # Newton's law of gravitation
+    force = (G * p1.mass * p2.mass)/(dis^2)
+    return [force*x, force*y]
+end
+
+function compute_forces(points::Array{Point}, quadTree::QuadTree, threshold::Float64)
+    forces_points = []
+    for p in 1:length(points)
+        sum_forces = [0.0, 0.0]
+        for i in 1:length(squares)
+            # we make sure that the quad tree has been initialized
+            if quadTree.squares[squares[i]] == nothing
+                continue
+            end
+
+            distance = euclidean_dis(points[p], quadTree.squares[squares[i]].point)
+            if distance < threshold
+                # we split the tree, and recurse into this tree
+                 force = compute_forces([points[p]], quadTree.squares[squares[i]], threshold)
+
+                 sum_forces[1] += force[1][1]
+                 sum_forces[2] += force[1][2]
+
+            else
+                # block point means there is no point there
+                if quadTree.point.x != -1
+                    force = attractive_force(points[p], quadTree.squares[squares[i]].point, distance) 
+                    sum_forces[1] += force[1]
+                    sum_forces[2] += force[2]
+                end 
+            end
+        end
+        push!(forces_points, sum_forces)
+    end
+
+    return forces_points
+end
+
+function simulate_movement(points, forces)
     return
 end
 
 # ----------------------------------------------------
-width = 100
-height = 100
+size = 100
+
+width = size
+height = size
+theta = size/2
 
 plane = Dict{String, Union{QuadTree, Nothing}}(
     "ul" => nothing,
@@ -148,12 +193,13 @@ plane = Dict{String, Union{QuadTree, Nothing}}(
 quad = QuadTree(plane, nothing, width, height, 0, 0)
 
 # Create a new points
-p = Point(20, 20, 20)
-p2 = Point(80, 80, 20)
+
 p1 = Point(20, 80, 20)
 p2 = Point(20, 70, 20)
 p3 = Point(80, 80, 20)
 p4 = Point(40, 10, 20)
+
+points = [p1, p2, p3, p4]
 
 println("p1")
 insert(quad, p1)
@@ -169,10 +215,14 @@ println("--------------------")
 # insert(quad, p3)
 
 # now let's calculate all barycenters
-# res = barycenter(quad)
+res = barycenter(quad)
 
 # compute the forces acting on every single point, and create new points out of them
-# point_forces = compute_forces(points, quad)
+forces = compute_forces(points, quad, theta)
+
+# WE ARE HERE
+# we apply the forces on our points, so we get a new set of points
+new_points = simulate_movement(points, forces)
 
 # save as into a gif. Repeat
 

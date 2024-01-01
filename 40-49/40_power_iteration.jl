4253af3579f892f760258bcbbfab767f76ba8d8a
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-01 09:53:17 +0100

    intermediary power iteration push

--- 40-49/40_power_iteration.jl
+++ 40-49/40_power_iteration.jl
@@ -1,30 +1,29 @@
 
 
 using Pkg
-Pkg.install("LinearAlgebra")
-Pkg.install("StatsBase")
+# Pkg.add("LinearAlgebra")
+# Pkg.add("StatsBase")
 using StatsBase, LinearAlgebra
  
 function get_random_matrix(size)
-    matrix = Matrix{T}(0, size, size)
+    matrix = zeros(Int, size, size)
 
     for i in 1:size
         for j in 1:size
-            matrix[i][j] = rand((0, 1))
+            matrix[i, j] = rand((0, 1))
         end
     end
 
     return matrix
 end
 
-function power_iteration(M, num_iterations: int = 100, d = 0.85)
+function power_iteration(M, num_iterations= 100, d = 0.85)
     N = size(M, 1)
     v = fill(1, N) / N
-    M_hat = (d.*M + (1 - d)/N)
+    M_hat = (M.*d .+ (1 - d)/N)
     
     for i in 1:num_iterations
         mul!(v, M_hat, v)
-        # v = M_hat @ v
     end
 
     return v
@@ -33,5 +32,6 @@ end
 size_network = 40
 
 the_network = get_random_matrix(size_network)
+println(the_network)
 result = power_iteration(the_network)
 println(result)

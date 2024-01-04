11b4b67f4e23b367b6e9b53a583fe903e008a3a4
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-04 13:21:07 +0100

    partially working version

--- 10-19/12_barnes_hut.jl
+++ 10-19/12_barnes_hut.jl
@@ -11,7 +11,7 @@ get_y(p) = p.y
 
 # will denote those trees that have not been initialized yet
 block_point = Point(-1,-1,-1)
-G = 6.674*10^(-1) # gravitational constant
+G = 6.674*10^(0) # gravitational constant
 
 
 mutable struct QuadTree
@@ -23,18 +23,29 @@ mutable struct QuadTree
 
     v_start::Float64
     h_start::Float64
+
+    barycenter::Point
 end
+
+plane_base = Dict{String, Union{QuadTree, Nothing}}(
+    "ul" => nothing,
+    "ur" => nothing,
+    "dl" => nothing,
+    "dr" => nothing
+)
 
 squares = ["ul", "ur", "dl", "dr"]
 
 function barycenter(quad::QuadTree)
-    if quad.point == nothing
+    # this is when a quad tree has not been used
+    if isnothing(quad.point)
         return true
     end
+
     top = float.([0, 0])
     bot = 0
     for i in eachindex(squares)
-        if quad.squares[squares[i]] != nothing
+        if !isnothing(quad.squares[squares[i]]) # not nothing
             barycenter(quad.squares[squares[i]])
 
             if quad.squares[squares[i]].point != nothing
@@ -46,13 +57,15 @@ function barycenter(quad::QuadTree)
         end
     end
 
+    # if none of the squares has any quad trees, we simply use the point
     if bot == 0
+        quad.barycenter = quad.point
         return quad.point
     end
 
     position = top / bot
-    quad.point = Point(position[1], position[2], bot)
-    return quad.point
+    quad.barycenter = Point(position[1], position[2], bot)
+    return quad.barycenter
 end
 
 
@@ -83,51 +96,52 @@ end
 
 function set_new_quad_tree(quad::QuadTree, point::Point, ud::String,lr::String)
     box = get_bounding_box(quad, ud, lr)
-    empty_dict = Dict{String, Union{QuadTree, Nothing}}(
-        "ul" => nothing,
-        "ur" => nothing,
-        "dl" => nothing,
-        "dr" => nothing
-        )
-    quad.squares[ud*lr] = QuadTree(empty_dict, point, box[1], box[2], box[3], box[4])
-    quad.point = block_point # an impossible point, so we block any processing here
+    empty_dict = copy(plane_base)
+
+    quad.squares[ud*lr] = QuadTree(empty_dict, nothing, box[1], box[2], box[3], box[4], block_point)
+    quad.point = point
 end
 
-l = 0
+# l = 0
 function insert(quad::QuadTree, point::Point)
     # very first point
-    # println("in insert")
-    if quad.point == nothing
+    # If the quad tree does not include a point and the point parameter, it means
+    # that the point can be placed here, instead of having to recurse downwards.
+    if isnothing(quad.point)
         quad.point = point
         return true
     end
 
+    # otherwise, we will need to create a new quad tree to accommodate our new point
+    # we first decide where in the current quad tree the upper point must go into, by
+    # getting each letter of the current point in the wuad tree
     ud_p = get_ud(quad, quad.point)
     lr_p = get_lr(quad, quad.point)
 
-    if quad.point.mass != -1
-        # println("quad.point")
-        # println(quad.point)
-        if quad.squares[ud_p*lr_p] == nothing
-            # println("setting new tree")
-            set_new_quad_tree(quad, quad.point, ud_p, lr_p)
-        else
-            # println("here")
-            insert(quad.squares[ud_p*lr_p], quad.point)
-        end
+    # we check if the sub tree already exists. If it doesn't, we place it there
+    if isnothing(quad.squares[ud_p*lr_p])
+        set_new_quad_tree(quad, quad.point, ud_p, lr_p)
+    else
+        # otherwise, we need to recurse into the next quad tree to do the same procedure
+        # println("here")
+        insert(quad.squares[ud_p*lr_p], quad.point)
     end
 
     ud = get_ud(quad, point)
     lr = get_lr(quad, point)
 
-    if quad.squares[ud*lr] == nothing
+    if isnothing(quad.squares[ud*lr])
         set_new_quad_tree(quad, point, ud, lr)
     else
         # println("there")
         insert(quad.squares[ud*lr], point)
     end
 
-    global l -= 1
+    # global l -= 1
+    # if l = 100
+    #     print("l: $l")
+    #     exit()
+    # end
 end
 
 function euclidean_dis(p1, p2)
@@ -136,8 +150,8 @@ end
 
 function attractive_force(p1::Point, p2::Point, dis::Float64)
     # normalize distances for x and y for unit vector
-    x = (p1.x + p2.x)/dis
-    y = (p1.y + p2.y)/dis
+    x = -(p1.x - p2.x)/dis
+    y = -(p1.y - p2.y)/dis
 
     # Newton's law of gravitation
     force = (G * p1.mass * p2.mass)/(dis^2)
@@ -146,31 +160,34 @@ end
 
 function compute_forces(points::Array{Point}, quadTree::QuadTree, threshold::Float64)
     forces_points = []
+
     for p in 1:length(points)
         sum_forces = [0.0, 0.0]
-        for i in 1:length(squares)
+        for i in eachindex(squares)
             # we make sure that the quad tree has been initialized
-            if quadTree.squares[squares[i]] == nothing
+            if isnothing(quadTree.squares[squares[i]])
                 continue
             end
 
-            distance = euclidean_dis(points[p], quadTree.squares[squares[i]].point)
+            distance = euclidean_dis(points[p], quadTree.squares[squares[i]].barycenter)
             if distance < threshold
                 # we split the tree, and recurse into this tree
                  force = compute_forces([points[p]], quadTree.squares[squares[i]], threshold)
-
+                #  println(force)
                  sum_forces[1] += force[1][1]
                  sum_forces[2] += force[1][2]
 
             else
                 # block point means there is no point there
                 if quadTree.point.x != -1
-                    force = attractive_force(points[p], quadTree.squares[squares[i]].point, distance) 
+                    force = attractive_force(points[p], quadTree.squares[squares[i]].barycenter, distance) 
+                    # println(force)
                     sum_forces[1] += force[1]
                     sum_forces[2] += force[2]
                 end 
             end
         end
+
         push!(forces_points, sum_forces)
     end
 
@@ -189,7 +206,7 @@ end
 # ----------------------------------------------------
 size = 100
 
-number_of_simulation_iterations = 14
+number_of_simulation_iterations = 100
 width = size
 height = size
 theta = size/2
@@ -201,53 +218,38 @@ plane_base = Dict{String, Union{QuadTree, Nothing}}(
     "dr" => nothing
 )
 
-plane = copy(plane_base)
-
-quad = QuadTree(plane, nothing, width, height, 0, 0)
 
 # Create a new points
-
 p1 = Point(20, 80, 20)
 p2 = Point(20, 70, 20)
 p3 = Point(80, 80, 20)
 p4 = Point(40, 10, 20)
 
-points = [p1, p2, p3, p4]
-
-# println("p1")
-# insert(quad, p1)
-# println("p2")
-# insert(quad, p2)
-# insert(quad, p3)
-# insert(quad, p4)
-# println("--------------------")
-# # println(quad.squares["ul"])
-# exit()
-
-# println("p3")
-# insert(quad, p3)
 
 temp_quad_tree = nothing
-temp_points = points
+temp_points = [p1, p2, p3, p4]
 points_of_points = []
 
 for i in eachindex(1:number_of_simulation_iterations)
     global temp_points
     global temp_quad_tree
-    # println(temp_points)
+    # println(plane_base)
     # exit()
-    # println("iteration: $i")
-    temp_quad_tree = QuadTree(copy(plane_base), nothing, width, height, 0, 0)
+    println("iteration: $i")
+    # println(block_point)
+    temp_quad_tree = QuadTree(copy(plane_base), nothing, width, height, 0, 0, block_point)
     # println("1")
-    println(length(temp_points))
+    # println(length(temp_points))
     for j in eachindex(temp_points)
-        # println("1$j")
-        # println(temp_points[j])
+        if temp_points[j].x > 100 || temp_points[j].y > 100
+            println("out of bounds. exiting")
+            exit()
+        end
         insert(temp_quad_tree, temp_points[j])
     end
     # gif(anim, "tutorial_anim_fps30.gif", fps = 30)
     # now let's calculate all barycenters
-    res = barycenter(temp_quad_tree)
+    barycenter(temp_quad_tree)
     # println("2")
     # compute the forces acting on every single point, and create new points out of them
     forces = compute_forces(temp_points, temp_quad_tree, theta)
@@ -268,7 +270,7 @@ anim = @animate for i = 1:number_of_simulation_iterations
     ylims!(0, 100)
 end
 
-gif(anim, "barnes_hut_1.gif", fps = 30)
+gif(anim, "barnes_hut_1.gif", fps = 15)
 
 # for i in 1:eachindex(new_points)
 #     insert(new_quad_tree, new_points[i])

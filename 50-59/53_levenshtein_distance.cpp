726561b125425a9b4de5569b7869535435e48520
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-10 08:35:51 +0100

    basic first draft levenshtein

--- 50-59/53_levenshtein_distance.cpp
+++ 50-59/53_levenshtein_distance.cpp
@@ -13,5 +13,6 @@ int main() {
     std::string a_string_complex = "yippie kay-yay!";
     std::string b_string_complex = "remember, remember, the fifth of november";
 
-    std::cout << levenshtein_distance() << std::endl;
+    std::cout << levenshtein_distance(a_string_simple, b_string_simple) << std::endl;
+    std::cout << levenshtein_distance(a_string_complex, b_string_complex) << std::endl;
 }

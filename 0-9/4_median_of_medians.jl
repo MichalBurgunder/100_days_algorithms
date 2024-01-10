406ada6cda590355a70de82d2de63a1b940a07be
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-10 07:43:17 +0100

    completion of mdian of medians algo

--- 0-9/4_median_of_medians.jl
+++ 0-9/4_median_of_medians.jl
@@ -5,31 +5,45 @@ using StatsBase
 function median_of_medians(array)
     temp = array
 
-    while size(temp)[1] > 1
+    still = ceil(size(array)[1]/2)
+    have_done = [0, 0]
+
+    while true
         pivot = rand((1, size(temp)[1]))
 
-        smaller = []
-        greater = []
+        left = []
+        right = []
+
+        if size(temp)[1] == 1
+            return temp[1]
+        end
+        
         for i in 1:size(temp)[1]
             if temp[i] < temp[pivot]
-                push!(smaller, temp[i])
+                push!(left, temp[i])
             else
-                push!(greater, temp[i])
+                push!(right, temp[i])
             end
         end
 
-        if size(smaller)[1] <= size(greater)[1]
-            temp = greater
+        if have_done[1] + size(left)[1] >= still
+            temp = left
+            have_done[2] = have_done[2] + size(right)[1]
+        elseif have_done[2] + size(right)[1] >= still
+            temp = right
+            have_done[1] = have_done[1] + size(left)[1]
         else
-            temp = smaller
+            return pivot 
         end
 
+        if size(temp)[1] == 0
+            return pivot
+        end
     end
-
-    return temp[1]
 end
 
 the_array = [75, 74, 24, 2, 22, 98, 65, 99, 4, 54, 102, 7, 35, 823, 45, 9, 87, 12, 1000, 67, 12]
 
-median = midpoint(the_array)
-println(median)
+median_result = median_of_medians(the_array)
+println(median_result)
+println(median(the_array))

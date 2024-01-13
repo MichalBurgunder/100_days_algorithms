67674083608448b149f68ef882ee74267984c3cf
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-13 12:43:47 +0100

    some cleaning

--- 90-99/Fisher_Yates.java
+++ 90-99/Fisher_Yates.java
@@ -3,11 +3,9 @@ import java.util.Arrays;
 
 public class Fisher_Yates {
     public static void main(String[] args) {
-        int[] ordered_array = {1,3,6,9,12,15,18,34,38,45,40,90,101,102,111,298,548,880,1000};
+        int[] ordered_array = {2, 4, 7, 9, 12, 12, 22, 24, 35, 45, 54, 65, 67, 74, 75, 87, 98, 99, 102, 823, 1000};
         
         int temp = 0;
-
-        System.out.println(ordered_array.length);
 
         for(int i = ordered_array.length-1; 0<i-1; i--) {
             Random rand = new Random();
@@ -16,8 +14,6 @@ public class Fisher_Yates {
             temp = ordered_array[new_position];
             ordered_array[new_position] = ordered_array[i];
             ordered_array[i] = temp;
-
-            System.out.println(Arrays.toString(ordered_array));
         }
         
         System.out.println(Arrays.toString(ordered_array));

0c9a1c3154faa41aabf0984b07cfd24a17d18afa
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-18 08:11:21 +0100

    sleep sort comments

--- 20-29/29_sleep_sort.js
+++ 20-29/29_sleep_sort.js
@@ -1,12 +1,24 @@
+// SLEEP SORT
 
+// we could also place them into an array if we wished to, although this is
+// just a formalization. Given that out we are working in a sync environment, we
+// would have to do some more processing (settimeout to the largest element
+// which then prints the array), but this is optional.
 function log_it(number) {
     console.log(number)
 }
 
+// we sort an array of integers by passing them into a sleep function, which
+// will execute a specific function (console.lo, above), that outputs the number.
+// Because the smallest numbers will sleep the shortest, they will be printed
+// out first. The longer ones will be printed out later, hence giving us an
+// ordered column of our integers.
 function sleep_sort() {
-    array = [2,34,6,4,67,9,8,12,3].map(element => element)
+    // our unsorted array
+    array = [3,17,6,2,14,9,8,12,4]
 
     for(let i=0;i<array.length;i++) {
+        // we set the timeout to sleep for the given amount of time
         setTimeout(log_it, array[i]*100, array[i])
     }
 }

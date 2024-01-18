910cab3eb56c3c92ca3b919cb1ea03c975392048
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-18 18:25:29 +0100

    sleep sort comments

--- 20-29/29_sleep_sort.js
+++ 20-29/29_sleep_sort.js
@@ -13,14 +13,15 @@ function log_it(number) {
 // Because the smallest numbers will sleep the shortest, they will be printed
 // out first. The longer ones will be printed out later, hence giving us an
 // ordered column of our integers.
-function sleep_sort() {
-    // our unsorted array
-    array = [3,17,6,2,14,9,8,12,4]
-
+function sleep_sort(array) {
+    // recall that we need to loop through the array to, at the very least,
+    // check every value
     for(let i=0;i<array.length;i++) {
         // we set the timeout to sleep for the given amount of time
         setTimeout(log_it, array[i]*100, array[i])
     }
 }
 
-sleep_sort()
+// our unsorted array
+array = [3,17,6,2,14,9,8,12,4]
+sleep_sort(array)

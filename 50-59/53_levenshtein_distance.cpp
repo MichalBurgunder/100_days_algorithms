224afd5202827d59b209e3933d2cc6af5e4ccff3
Michal Burgunder <michal_burgunder@yahoo.com>
2024-01-19 13:34:14 +0100

    first complete version levenshtein distance

--- 50-59/53_levenshtein_distance.cpp
+++ 50-59/53_levenshtein_distance.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// we get the minimum entry of a particular array
 int get_min_element(int the_array[]) {
     int the_int = INFINITY;
 
@@ -19,15 +20,17 @@ int levenshtein_distance(int i,
                             int j,
                                 std::vector<std::vector<int> > *arr) {
 
-    int cost = (*arr)[i-1][j-1] == (*arr)[i][j] ? 0 : 1;
-
+    // because the character is different, we need to take the minimum distance
+    // for the substrings, and add 1, as the cost to change this character. This
+    //  automatically considers all possible changes to the previous substrings,
+    //  and only outputs their Levenshteins distances:
     int integer_array[] = {
         (*arr)[i-1][j]+1,
         (*arr)[i][j-1]+1,
-        (*arr)[i-1][j-1]+cost
+        (*arr)[i-1][j-1]+1
     };
-    int res = get_min_element(integer_array);
 
+    // and because we want the smallest number, we get the minmum of this array
     return get_min_element(integer_array);
 }
 
@@ -52,31 +55,55 @@ int main() {
     std::vector<std::vector<int> > dynamic_array(len_a,
                                         std::vector<int>(len_b, -1));
 
-    // if the first character of the string is same,
+    // we check if the first character of the string is same, thus initializing
+    // our rectangle of subresults.
     dynamic_array[0][0] = string_a[0] == string_b[0] ? 0 : 1;
 
-    // first, we fill in those values that obvious: when string b is "" and all
-    // substrings of a. This distance will always be the length of the substring
-    // of a, as one would have to add a new character to b for every character
-    // in a. In other words, the distance is always the length of the substring. 
+    // we begin filling in those values in the array that obvious: when string_b
+    // is "" and all substrings of a. This distance will always be the length of
+    // the substring of a, as one would have to add a new character to b for
+    // every character in a. In other words, the distance is always the length
+    // of the one substring. 
     for (int i = 1; i < len_a; i++) {
         dynamic_array[i][0] = i + dynamic_array[0][0];
     }
 
-    // we perform the same operation, but now when string a is empty,
-    // while we cycle through the substrings of b
+    // we perform the same operation, but now when string a is empty, while we
+    // cycle through the substrings of b
     for (int j = 1; j < len_b; j++) {
         dynamic_array[0][j] = j + dynamic_array[0][0];
     }
     
 
+    // now we fill out the more difficult values, starting with the simplest
+    // next version: when at least one string as two characters. Every step is
+    // filled out in the same manner: we check the "top", "left" and "diagonal
+    // top-left" values. That is, we check the Levenshtein distances of those
+    // substrings that are above, to the left, and diagonally to the top-left
+    // from our current position":
+    //
+    //     h   a   ...
+    //   ---------
+    // h | 0 | 1 | ...
+    //   ----\----
+    // e | 1 | x | ...
+    // .   .   .
+    // .   .   .
+    // .   .   .
+
+    // because we fill out the table row by row, we "chase" the x corner across 
+    // he matrix, because we are capable of filling it our.
 
     for (int i = 1; i < len_a; i++) {
         for (int j = 1; j < len_b; j++) {
 
-            if (string_a[i - 1] == string_b[j - 1]) {
+            // in the case where the current character is the same, the
+            // Levenshtein distance does not change. Thus, we only take the
+            // value from the previous string comparison 
+            if (string_a[i] == string_b[j]) {
                 dynamic_array[i][j] = dynamic_array[i - 1][j - 1];
             } else {
+                // otherwise, we need to actually fill it out
                 dynamic_array[i][j] = levenshtein_distance(i,
                                                             j, 
                                                             &dynamic_array);

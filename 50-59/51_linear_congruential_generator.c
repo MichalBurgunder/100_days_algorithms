7ab7f0ea006ea2faef74ccca5a35d9bf9d2f3c9a
Michal Burgunder <michal_burgunder@yahoo.com>
2024-03-22 17:51:26 +0100

    linear congruetial generator first full draft

--- 50-59/51_linear_congruential_generator.c
+++ 50-59/51_linear_congruential_generator.c
@@ -2,6 +2,16 @@
 
 #include <stdio.h>
 
+// Here is the ACUTAL linear congruential generator. A practical one relies on
+// saving x for further random numbers, which here, we temporarily save in the
+// generate_random_numbers() function. 
+long get_random_number(unsigned long x, unsigned long a, unsigned long c,
+                                                            unsigned long m) {
+    return (a * x + c) % m;
+}
+
+// to avoid having to redefine variables, I just plugged them into this function
+// for cleaner management of constants. 
 void reset_nums(
     unsigned long *x, unsigned long *a, unsigned long *c, unsigned long *m, 
             long new_x, long new_a, long new_c, long new_m) {
@@ -9,18 +19,24 @@ void reset_nums(
     *a = new_a;
     *c = new_c;
     *m = new_m;
-
 }
 
-long get_random_number(unsigned long x, unsigned long a, unsigned long c, unsigned long m) {
-    return (a * x + c) % m;
-}
-
-void generate_random_numbers(unsigned long x, unsigned long a, unsigned long c, unsigned long m, int normalize) {
+// This function is mostly just a wrapper for easier demonstration, with the
+// exception that it saves the x number for future use/for generating additional
+// random numbers.
+void generate_random_numbers(unsigned long x, unsigned long a, unsigned long c,
+                                            unsigned long m, int normalize) {
+    // we here save our current random number, that we plug into the linear
+    // congruetial generator. The output from that function updates this
+    // rolling x
     long rolling_number = x;
 
-    for(int i=0;i<20;i++) {
+    // for demonstration purposes, we only generate the first 20 random numbers
+    for(int i = 0; i < 20; i++) {
+        // we take all of our constants & x, and plug them in
         rolling_number = get_random_number(rolling_number, a, c, m);
+
+        // some code for neat printing
         if(normalize == 1) {
             float normalized = (float)rolling_number / m;
             printf("%.2f ", normalized);
@@ -38,19 +54,36 @@ int main() {
     unsigned long c = 0;
     unsigned long m = 2;
 
-    // With very simple paramters the results can be very predictable to the human eye
+    // With very simple paramters the results can be very predictable. In this
+    // case, when c = 0, and a is a multiple of m, the result always evaluates
+    // to 0
     generate_random_numbers(x, a, c, m, 0);
 
-//  With a little more 'complex' numbers, we can get seemingly more random results. However, on closer inspection, they are in fact a recurring set of numbers. \n");
+    // With larger numbers, and ones that are relatively prime to one another
+    // (a and m), we can get a better results. However, on inspection, they are
+    // in fact a small recurring set of numbers within the modular function.
+    // Thus, knowing one number will allow us to easily predict the next number
+    // to be generated.
     reset_nums(&x, &a, &c, &m, 2, 5, 2, 7);
     generate_random_numbers(x, a, c, m, 0);
 
-// Increasing the size of the modulator will increase the size of the loop. Furthermore, by changing up the paramters to larger numbers, it more difficult to guess the 'seed' of the random number generator function
+    // Increasing the size of the modulator will increase the size of the loop.
+    // Furthermore, by changing 'a' to a larger numbers, it more difficult to
+    // guess the 'seed' (starting conditions) of the random number generator
+    // function. With the large numbers below, it will take many queries to the
+    // random number generator before sequences repeat. Many, yes, but not
+    // unrealistically many.
     reset_nums(&x, &a, &c, &m, 103, 778, 935, 1081);
     generate_random_numbers(x, a, c, m, 0);
 
-// Finally, if we increase the number to very large numbers, then normalize on [0, 1], we get a seemingly completely random number generator, even though these number are in fact, pseudorandom.\n");
-    reset_nums(&x, &a, &c, &m, 41293834263, 9832629238, 23421341234123, 923849234902);
+    // Finally, if we increase all numbers to very large sizes, the loop becomes
+    //  so large that it takes a significant amount of time before the function
+    // loops again. Ideally, the loop would be so large that it would take more
+    // than a few minutes, days, or weeks before the loop repeats, when
+    // resources are full dedicated to generating these random numbers. If we
+    // additionally normalize these numbers on [0, 1], we can get easily
+    // intrepretable numbers.
+    reset_nums(&x, &a, &c, &m, 4129383429, 9826292137, 232134123423, 922349401);
     generate_random_numbers(x, a, c, m, 1);
     return 0;
 }

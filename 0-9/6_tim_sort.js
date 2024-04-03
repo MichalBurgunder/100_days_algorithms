ec9c3a89e18dca6c3a946c37abf292db5441c9f7
Michal Burgunder <michal_burgunder@yahoo.com>
2024-04-03 14:24:11 +0200

    working implementation timsort

--- 0-9/6_tim_sort.js
+++ 0-9/6_tim_sort.js
@@ -1,15 +1,17 @@
+// TIM SORT ALGORITHM
 
-
-const MINIMUM = 2
+const MINRUN = 2
 
 function find_minrun(n) {
     let r = 0
-    while(n >= MINIMUM) {
+    while(n >= MINRUN) {
         r |= n & 1
         n >>= 1 
     }
     return n + r 
 }  
+
+// here we are simply doing an insertion sort: go through the array, element by element, switching those positions that need switching, until the full array is sorted)
 function insertion_sort(array, left=0, right=array.length) {
     let temp = null
     
@@ -27,64 +29,39 @@ function insertion_sort(array, left=0, right=array.length) {
 
 
 function merge(array, l, r, e) { 
-    i = l
-    j = r
 
-    left = array.slice(l, r)
-    right = array.slice(r+1, e)
-    
-    new_array = [...array]
-    
-    while(true) {
-        if (i >= r && j >= array.length) {
-            break
-        }
 
-        if ((array[i] < array[j] && i != r) || j > 2*l-r) {
-            new_array[i+j-r] = array[i]
+    left = array.slice(l, r+1)
+    right = array.slice(r+1, e+1)
+
+    let i = 0
+    let j = 0
+    let setPosition = l
+    while (i < left.length && j < right.length) {
+        console.log()
+        if (left[i] < right[j]) {
+            array[setPosition] = left[i]
             i++
+            setPosition++
         } else {
-            new_array[i+j-r] = array[j]
+            array[setPosition] = right[j]
             j++
+            setPosition++
         }
-
+        console.log("array")
+        console.log(array)
     }
 
-    return new_array
-    // for(let i=0;i< array_length1;i++) {
-    //     left.push(array[l + i]) 
-    // } 
 
-    // for(let i=0;i<array_length2;i++) {
-    //     right.push(array[m + 1 + i]) 
-    // } 
-
-    // i=0
-    // j=0
-    // k=l
-   
-    // while(j < array_length2 && i < array_length1) {
-    //     if(left[i] <= right[j]) {
-    //         array[k] = left[i] 
-    //         i += 1
-    //     } else {
-    //         array[k] = right[j] 
-    //         j += 1
-    //     }
-    //     k += 1
-    // }
-
-    // while (i < array_length1) {
-    //     array[k] = left[i] 
-    //     k += 1
-    //     i += 1
-    // } 
-
-    // while(j < array_length2) {
-    //     array[k] = right[j] 
-    //     k += 1
-    //     j += 1
-    // }
+    while (i < left.length) {
+        array[setPosition] = left[i]
+        i++, setPosition++
+    }
+    
+    while (j < right.length) {
+        array[setPosition] = right[j]
+        j++, setPosition++
+    }
 }
 
 // timsort combines different sorting algorithms into a single one, such that we
@@ -102,53 +79,36 @@ function tim_sort(array) {
 
     for(let i=0;i<n;i+=minrun) {
         end = Math.min(i + minrun - 1, n - 1) 
-        // console.log(i)
-        // console.log(end)
         insertion_sort(array, i, end) 
     }
     
-    // console.log(minrun)
-    let new_array = array 
-    console.log(new_array)
     size = minrun
-    while(size < n+1) {
-        for(let i = 0; i < n; i++) {
-            let left = Math.min(i*size, n - 1) 
-            let right = Math.min(((i+1)*size), (n - 1)) 
-            // print()
-            console.log("----")
-            console.log(left, right)
-            // console.log(right)
-            // console.log("----")
-            console.log(new_array)
-            console.log("STARTING")
-            new_array = merge(new_array, left, right) 
-            console.log(new_array)
-            console.log("----")
-            process.exit()
-            // console.log("skdrjgisudrg")
-            // print(2*size)
+
+    for (let size = minrun; size < n+1; size *= 2) {
+        for (let left = 0; left < n; left += 2 * size) {
+            const mid = left + size - 1;
+            const right = Math.min(left + 2 * size - 1, n - 1);
+
+            merge(array, left, mid, right);
+
         }
-        console.log(new_array)
-        process.exit()
-        process.exit()
-        size = 2 * size 
     }
-    return new_array
+
+    return array
 }
   
   
 array = [-1,5,0,-3,11,9,-2,7,0]
-array2 = [-1,5,0,-3,11,9,-2,7,0]
+// array = [-1,5,0,-3]
+// array = [1,8,99,100,6,9,40]
 
 // we measure the time it takes for timsort an the js native sort
 console.time()
-// console.log(tim_sort(array))
-// console.log(insertion_sort(array2))
-console.log(merge([1,8,99,2,7,40,8], 0, 4))
-//  res = merge([1,2,8,3,5,7], 0, 3)
-//  console.log(res)
-// console.log(merge(res, 0, 3))
+console.log(tim_sort(array))
+console.timeEnd()
+
+console.time()
+console.log(array.sort())
 console.timeEnd()
 
 // res = 

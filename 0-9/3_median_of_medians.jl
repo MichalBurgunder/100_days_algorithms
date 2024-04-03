5ae725e6d9bc231917831527b2df7eff10b451fd
Michal Burgunder <michal_burgunder@yahoo.com>
2024-04-03 14:31:37 +0200

    median of medians diagram

--- 0-9/3_median_of_medians.jl
+++ 0-9/3_median_of_medians.jl
@@ -3,7 +3,38 @@
 using Pkg
 using StatsBase
 
+#                           array_1
+#     [k_1 , k_2,  k_3,  k_4 ... k_(n-3) k_(n-2) k_(n-1) k_n]
+#                /                        \ 
+#         [k_i, ... k_m]   PIVOT_1   [k_j, ... k_n]
+#                |
+#                |
+#                |
+#             array_2
+#
+# set new array, take out some random PIVOT_2 element from new array
+#                           array_2
+#           [k_1, k_2, k_3    ...   k_(m-2), k_(m-1), k_(m)]
+#                  /                        \ 
+#          [k_1 ... k_(m_2)] PIVOT_2 [k_j, ... k_(n_2)]
+#                  /                        \ 
+#                ...                        ...
+#                |
+#             array_3
+#                           array_3
+#                  /                        \ 
+#               ...                         ...
+#                                            |
+#                             ...
+# 
+#                           Median:
+#                              X
+# until finally, a pivot emerges, that must forcibly be the median
+# 
 function median_of_medians(array)
+    # the overall idea is make the search space smaller by n/2 at every step,
+    # until we've found the median. If we search the array in such a way, we
+    # take n + n/2 + n/4 + n/6 + ... = 2n --> O(n) time to do it. 
     temp = array
 
     still = ceil(size(array)[1]/2)
@@ -29,12 +60,11 @@ function median_of_medians(array)
         left = []
         right = []
 
-
         
-        # here we sort our given array, with the pivot with smaller/bigger
-        # elements. If this is the first time we pass into this code, we know
-        # that the median must be in the bigger array, which we recursively will
-        # go into
+        # here we sort our given array against the pivot with smaller/bigger
+        # elements (O(n) time). We know that the median will be in that subarray
+        # that is bigger than half of the original array size, at each
+        # iteration. 
         for i in 1:size(temp)[1]
             if temp[i] < temp[pivot]
                 push!(left, temp[i])
@@ -47,9 +77,9 @@ function median_of_medians(array)
         # elements we still need on the right & left, before a global median
         # emerges. If both sides have the correct amount of elements (1/2 of
         # the original array), then we know that the pivot itself must be the
-        # median, and return this instead. For arrays of even numbers, we pick
-        # out the left number (note that the Julia implementation takes the mean
-        # of the two)
+        # median, and return this instead. For arrays of even numbers, and a
+        # tie-break, we pick out the left number (note that our Julia
+        # implementation takes the mean of the two)
         if have_done[1] + size(left)[1] >= still
             temp = left
             have_done[2] = have_done[2] + size(right)[1]
@@ -62,6 +92,7 @@ function median_of_medians(array)
     end
 end
 
+# this algorithm works with any array
 the_array = [75, 74, 24, 2, 22, 98, 65, 99, 4, 54, 102, 7, 35, 823, 45,
                                                         9, 87, 12, 1000, 67, 12]
 

8378600a8be4849119d819f7c72eb98e2cde1213
Michal Burgunder <michal_burgunder@yahoo.com>
2024-04-09 10:38:20 +0200

    next level runge kutta

--- 80-89/86_runge_kutta_methods.jl
+++ 80-89/86_runge_kutta_methods.jl
@@ -1,33 +1,54 @@
 # RUNGE-KUTTA METHOD
+using Pkg
+# Pkg.add("Plots")
+using Plots
 
-# function
-# (1/32)*x^5+(1/60)x^4-(21/50)x^3+0.8
+function actual_function(x)
+    return (1/32)*x^5+(1/60)*x^4-(21/50)*x^3+0.8*x
+end
 
 # derivative
 # \frac{5}{32}x^{4}+\frac{4}{60}x^{3}-\frac{63}{50}x^{2}+0.8
 
-function derivative_function(t)
-    return (5/32)*t^4+(4/60)*t^3-(63/50)*t^2+0.8
+function derivative_function(x)
+    return (5/32)*x^4+(4/60)*x^3-(63/50)*x^2+0.8
 end
 
 function runge_kutta()
-    y_n = [0.316]
-    h = 0.1
+    x_n = [-3, -2, -1, 0, 1, 2, 3]
+    predicted_yn = [0.316]
+    actual_function_yn = [0.316]
 
-    for k in range(0, 50, 1)
-        t = k/10-2
+    x_ending = 3 
+    x_increment = 0.01
+    x_beginning = -3+x_increment
+
+    y_beginning = 0.316 # only need this for vertical shift
+
+    final_ts = [-3.0]
+    for t in x_beginning:x_increment:x_ending
+        push!(final_ts, t)
+        # println(t)
+
+        i = size(predicted_yn)[1]
         k1 = derivative_function(t)
+        k2 = derivative_function(t+x_increment/2) + predicted_yn[i]+x_increment*(k1/2)
+        k3 = derivative_function(t+x_increment/2) + predicted_yn[i]+x_increment*(k2/2)
+        k4 = derivative_function(t+x_increment) +   predicted_yn[i]+x_increment*k3
 
 
-        k1 = derivative_function()
-        k2 = derivative_function(t+h/2) + y_n[k]+h*(k1/2)
-        k3 = derivative_function(t+h/2) + y_n[k]+h*(k2/2)
-        k4 = derivative_function(t+h) +   y_n[k]+h*k3
+        y_n_plus_1 = predicted_yn[i] + x_increment/6*(k1+2*k2+2*k3+k4)
 
-
-        y_n_plus_1 = y_n[k] + h/6*(k1+k2+k3+k4)
-
-        push!(y_n_plus_1)
+        push!(predicted_yn, y_n_plus_1)
+        push!(actual_function_yn, actual_function(t))
     end
+
+    # println(predicted_yn)
+    display(plot(final_ts, [actual_function_yn, predicted_yn]))
+    # plot(final_ts, predicted_yn)
+    # display(plot(final_ts, [predicted_yn, actual_function_yn]))
+    wait()
 end
 
+
+runge_kutta()

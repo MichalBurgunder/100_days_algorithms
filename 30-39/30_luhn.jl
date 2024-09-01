b0d050138bbc4dd88d04a5fc26aaecaf97253435
Michal Burgunder <michal_burgunder@yahoo.com>
2024-09-01 15:26:29 +0200

    luhn comments

--- 30-39/30_luhn.jl
+++ 30-39/30_luhn.jl
@@ -1,5 +1,6 @@
 # LUHN ALGORITHM
 
+# while this is the implementation of the Luhn algorithm, it gives us a prototype on which to other identification number checkers. For example, there is no reason why the checksum should add to 0 instead of, say, 6. We also do not need to multiply by two: we can multipl by 3, 4, 9384, etc., as long as we reduce the result of the multiplication/function to a single sigit in a deterministic way. So lines 17-22 can be replaced by a function of any complexity that fulfills these requirements. This is why identification numbers on ID, bank numbers, etc. cannot be "hacked" all too easily: only the issuing authority can detemrine whether the number given is a valid number of not.  
 function luhn_algorithm(string_number)
     sum = 0
     number_of_digits = length(string_number)

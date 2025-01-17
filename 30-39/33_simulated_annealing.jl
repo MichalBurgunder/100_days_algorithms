ba7f027ef38c351a953944889d949378a56f0ed2
Michal Burgunder <michal_burgunder@yahoo.com>
2025-01-17 19:09:48 +0100

    annealing first working version

--- 30-39/33_simulated_annealing.jl
+++ 30-39/33_simulated_annealing.jl
@@ -28,35 +28,46 @@ function energy_difference(grid, s1, s2)
 
     grid_size = size(grid)
     original_dir = s1 .- s2
+    original_dir_negative = s2 .- s1
 
-
+    divisor_s1 = 4
     for i in range(1, size(directions)[1])
-        if is_valid_grid_position(s1 .+ directions[i], grid_size)
-            energy_s1 += abs(grid[s1[1], s1[2]] - grid[s1[1]+directions[i][1], s1[2]+directions[i][2]])
+        if is_valid_grid_position(s2 .+ directions[i], grid_size) && directions[i] !== original_dir_negative
+            energy_s1 += abs(grid[s1[1], s1[2]] - grid[s2[1]+directions[i][1], s2[2]+directions[i][2]])
+        else
+            divisor_s1 -= 1
         end
     end
 
+    divisor_s2 = 4
     for i in range(1, size(directions)[1])
-        if is_valid_grid_position(s2 .+ directions[i], grid_size)
-            energy_s2 += abs(grid[s2[1], s2[2]] - grid[s2[1]+directions[i][1], s2[2]+directions[i][2]])
+        if is_valid_grid_position(s1 .+ directions[i], grid_size) && directions[i] !== original_dir
+            energy_s2 += abs(grid[s2[1], s2[2]] - grid[s1[1]+directions[i][1], s1[2]+directions[i][2]])
+        else
+            divisor_s1 -= 1
         end
     end
 
-    return energy_s1 + energy_s2 - 2*abs(grid[s1[1], s1[2]]-grid[s2[1], s2[2]])
+    # println(abs(energy_s1/divisor_s1 + energy_s2/divisor_s2))
+
+    return energy_s1/divisor_s1 + energy_s2/divisor_s2
 end
 
 
 function pick_new_state(state, grid_size)
+    return [rand(range(1, grid_size[1])), rand(range(1, grid_size[2]))]
+
     while(true)
         new_state = state .+ rand(directions)
 
         if is_valid_grid_position(new_state, grid_size) 
-            return new_state
+            return new_state, direction
         end
     end
 end
 
-function simulated_annealing(grid, steps=100)
+
+function simulated_annealing(grid, steps)
     height = size(grid)[1]
     width = size(grid)[2]
 
@@ -68,14 +79,16 @@ function simulated_annealing(grid, steps=100)
     state = [Int(height/2), Int(width/2)]
 
     for i in range(0, steps)
-        T = 1 - (i+1)/steps
+        T = (i+1)/steps
         
         new_state = pick_new_state(state, grid_size)
 
-        if energy_difference(grid, state, new_state) > rand(Float64)
-            temp = state
-            state = new_state
-            new_state = temp
+        # if true, we swap the two positions
+        if energy_difference(grid, state, new_state) > rand(Float64) + T
+            temp = grid[state[1], state[2]]
+            grid[state[1], state[2]] = grid[new_state[1], new_state[2]]
+            grid[new_state[1], new_state[2]] = temp
+
         end
     end
 
@@ -97,13 +110,14 @@ function convert_to_Gray(maze)
 end
 
 
-height = 150
-width = 150
-steps = 100000
+height = 50
+width = 50
+steps = 1000000
 
 grid = rand(range(0, 255)/255, height, width)
 grid_gray = convert_to_Gray(grid)
 grid_gray = map(clamp01nan, grid_gray)
+
 save("./i1.bmp", grid_gray)
 
 grid_gray_sim = simulated_annealing(grid, steps)

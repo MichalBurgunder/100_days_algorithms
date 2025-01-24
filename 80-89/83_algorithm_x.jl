a7e32e2c7a25deea0c7afa9cc6664a16e468ecad
Michal Burgunder <michal_burgunder@yahoo.com>
2025-01-24 13:39:04 +0100

    first draft algorithm X

---
+++ 80-89/83_algorithm_x.jl
@@ -0,0 +1,73 @@
+# ALGORITHM X
+function algorithm_x(adjecency_matrix, excluded_rows=[]) 
+    sums_of_ones = []
+    for i in 1:size(adjecency_matrix)[1]
+        sums_of_ones[i] = sum(adjecency_matrix[i])
+    end
+
+    minimum_number = min(sums_of_ones)
+    rows = []
+
+    for i in 1:size(adjecency_matrix)[1]
+        if !(i in excluded_rows) && sums_of_ones[i] == minimum_number
+            push!(rows, i)
+        end
+    end
+
+    while(true)
+        
+        row_to_choose = rnd(rows)
+        rows = filter!(element -> element != row_to_choose, a)
+        
+        rows_to_exclude = []
+        for i in 1:size(adjecency_matrix)[1]
+            if sums_of_ones[row_to_choose,i] == 1
+                for j in 1:size(adjecency_matrix)[1]
+                    if i != j && adjecency_matrix[i,j] == 1
+                        push!(rows_to_exclude, j)
+                    end
+                end
+            end
+        end
+    
+        all_excluded_rows = collect(set(vcat(excluded_rows, rows_to_exclude)))
+        if size(all_excluded_rows)[1] == size(adjecency_matrix)[1]
+            return all_excluded_rows
+        else
+end
+
+# converts an edge list to an adjacency matrix. Of course, we could have chosen to simply written the adjacency matrix and be done. However, with an edge list, we can very clearly see what nodes are connected to what other nodes as opposed to the matrix structre. This way we can also more easily modify our graph, in a coherent way. 
+function convert_to_adjacency_matrix(nodes, edge_list)
+    matrix = zeros(size(nodes)[1])
+    for i in 1:size(nodes)[1]
+        for j in 1:edge_list[i]
+            matrix[i,j] = 1
+        end
+    end
+    return matrix
+end
+
+# the graph for which we are trying to find an exact cover. While this is very much a toy problem (solution is {2, 6, 10}) it is fully understandable as a problem and solution.
+
+#  1 -- 2 -- 3
+#       | 
+#       4 -- 5 -- 6 -- 7
+#                 | 
+#      10 -- 9 -- 8 
+
+nodes = [1:1:10;]
+sets = [
+    [2],     # 1
+    [1,3,4], # 2
+    [2],     # 3
+    [2,5],   # 4
+    [4,6],   # 5
+    [5,7,8], # 6
+    [6],     # 7
+    [6,9],   # 8
+    [8,10],  # 9
+    [9]     # 10
+]
+
+adjacency_matrix = convert_to_adjacency_matrix(nodes, sets)
+result = algorithm_x(adjacency_matrix)

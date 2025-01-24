4d73e327479101522a4761e571f2ef6125b89b33
Michal Burgunder <michal_burgunder@yahoo.com>
2025-01-24 13:58:42 +0100

    first full version algorithm x

--- 80-89/83_algorithm_x.jl
+++ 80-89/83_algorithm_x.jl
@@ -1,4 +1,30 @@
 # ALGORITHM X
+
+
+
+function where_row_is_minimum_number(number_of_nodes, excluded_rows, sums_of_ones, minimum_number)
+    return [filter(element -> i in excluded_rows) || sums_of_ones[i] != minimum_number for i in 1:number_of_nodes]
+end
+
+function exclude_rows(adjacency_matrix, number_of_nodes, sums_of_ones)
+    rows_to_exclude = []
+
+    for i in 1:number_of_nodes
+        if sums_of_ones[row_to_choose,i] == 1
+            for j in 1:number_of_nodes
+                if i != j && adjacency_matrix[i,j] == 1
+                    push!(rows_to_exclude, j)
+                end
+            end
+        end
+    end
+    return rows_to_exclude
+end
+
+function concatenate_arrays(a, b)
+    return collect(set(vcat(a, b)))
+end
+
 function algorithm_x(adjecency_matrix, excluded_rows=[]) 
     sums_of_ones = []
     for i in 1:size(adjecency_matrix)[1]
@@ -6,34 +32,30 @@ function algorithm_x(adjecency_matrix, excluded_rows=[])
     end
 
     minimum_number = min(sums_of_ones)
-    rows = []
+    number_of_nodes = size(adjecency_matrix)[1]
+    rows = where_row_is_minimum_number(number_of_nodes, excluded_rows, sums_of_ones, minimum_number)
 
-    for i in 1:size(adjecency_matrix)[1]
-        if !(i in excluded_rows) && sums_of_ones[i] == minimum_number
-            push!(rows, i)
-        end
-    end
 
     while(true)
-        
-        row_to_choose = rnd(rows)
-        rows = filter!(element -> element != row_to_choose, a)
-        
-        rows_to_exclude = []
-        for i in 1:size(adjecency_matrix)[1]
-            if sums_of_ones[row_to_choose,i] == 1
-                for j in 1:size(adjecency_matrix)[1]
-                    if i != j && adjecency_matrix[i,j] == 1
-                        push!(rows_to_exclude, j)
-                    end
-                end
-            end
+        if size(rows)[1] == 0
+            return excluded_rows
         end
-    
-        all_excluded_rows = collect(set(vcat(excluded_rows, rows_to_exclude)))
-        if size(all_excluded_rows)[1] == size(adjecency_matrix)[1]
+        row_to_choose = rnd(rows)
+        filter!(element -> element != row_to_choose, rows)
+        
+        rows_to_exclude = exclude_rows(adjacency_matrix, number_of_nodes, sums_of_ones)
+
+        all_excluded_rows = concatenate_arrays(excluded_rows, rows_to_exclude)
+
+        if size(all_excluded_rows)[1] == number_of_nodes
             return all_excluded_rows
-        else
+        end
+
+        result = algorithm_x(adjecency_matrix, all_excluded_rows)
+        if size(result)[1] == size(adjecency_matrix)[1]
+            return result
+        end
+    end
 end
 
 # converts an edge list to an adjacency matrix. Of course, we could have chosen to simply written the adjacency matrix and be done. However, with an edge list, we can very clearly see what nodes are connected to what other nodes as opposed to the matrix structre. This way we can also more easily modify our graph, in a coherent way. 

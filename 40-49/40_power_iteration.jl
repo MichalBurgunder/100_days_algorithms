5b51ea401b357f6282377981ceb946ae91c68aea
Michal Burgunder <michal_burgunder@yahoo.com>
2025-04-11 16:56:09 +0200

    final first version power iteration

--- 40-49/40_power_iteration.jl
+++ 40-49/40_power_iteration.jl
@@ -5,11 +5,17 @@ using Pkg
 # Pkg.add("StatsBase")
 using StatsBase, LinearAlgebra
 
-# although a significant portion of Google's PageRank algorithm, this portion of the algorithm has been made public, given the advances that Google's search engine has made after the implementation fo the algorithm. 
+# although a significant portion of Google's PageRank algorithm, it has
+# nonetheless been made public, as that Google's search engine has advances
+# far beyond it.
 function power_iteration(M, num_iterations= 50)
     N = size(M, 1)
 
-    # we take a random vector of size N. It doesn't much matter what vector this is: because we multiply it by a matrix, and then norm the result (a constant multiplier), the product of all the norms effectively act as the eigenvalue of the matrix, leaving behind the eigenvector, which characterizes the network in question. 
+    # we take a random vector of size N. It doesn't much matter what vector we
+    # define: because we multiply it by a matrix, and then divide it element-
+    # wise by the norm, the product of all the norms effectively act as
+    # mulitplier (i.e. an eigenvalue), leaving behind the eigenvector, which
+    # characterizes the network in question. 
     v = Float64.(rand((Bool), 1, N))
 
     v_result = similar(v)
@@ -25,7 +31,12 @@ function power_iteration(M, num_iterations= 50)
     return v
 end
 
-# a simple network, where the rows signify links going out of row_number row, and going into the column_number node. What's important to note here, is that the node_5 has few outgoing connections, but many incoming ones. This node would then ran high in a ranking of best webpages. On the other hand, node_2 only has  single link going to it, which means that it is a rather unpopular website. So this webpage has a low ranking among the other websites.
+# a simple network, where the rows signify links going out of the [row_number]th
+# node, and going into the [column_number]th node. What's important to note
+# here, is that the node_5 has few outgoing connections but many incoming ones.
+# This node would then rank high in a ranking of best webpages. On the other
+# hand, node_2 only has single link going to it, which means that it is a rather
+# unpopular website. So this webpage has a low ranking among the other websites.
 the_network = Matrix(
     [
         0 0 0 0 1;

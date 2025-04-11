fc529aa60e5069239d428420738fe2809a27d05a
Michal Burgunder <michal_burgunder@yahoo.com>
2025-04-11 13:54:56 +0200

    first working version power iteration

--- 40-49/40_power_iteration.jl
+++ 40-49/40_power_iteration.jl
@@ -4,34 +4,38 @@ using Pkg
 # Pkg.add("LinearAlgebra")
 # Pkg.add("StatsBase")
 using StatsBase, LinearAlgebra
- 
-function get_random_matrix(size)
-    matrix = zeros(Int, size, size)
 
-    for i in 1:size
-        for j in 1:size
-            matrix[i, j] = rand((0, 1))
-        end
-    end
 
-    return matrix
-end
-
-function power_iteration(M, num_iterations= 100, d = 0.85)
+function power_iteration(M, num_iterations= 50)
     N = size(M, 1)
-    v = fill(1, N) / N
-    M_hat = (M.*d .+ (1 - d)/N)
-    
+    v = Float64.(rand((Bool), 1, N))
+
+    v_result = similar(v)
+
     for i in 1:num_iterations
-        mul!(v, M_hat, v)
+        
+        mul!(v_result, v, M)
+
+        v_normed = v_result / norm(v_result)
+        v = v_normed
     end
 
     return v
 end
 
-size_network = 40
+# 
+the_network = Matrix(
+    [
+        0 0 0 0 1;
+        0 0 1 1 1;
+        1 0 0 1 1;
+        1 0 0 0 1;
+        0 0 1 0 0
+    ]
+)
 
-the_network = get_random_matrix(size_network)
-println(the_network)
 result = power_iteration(the_network)
-println(result)
+
+for i in 1:size_network
+    println(result[i])
+end
